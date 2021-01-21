1b381d34f180fd8ff0aa6b22c2af2bccee319137
swisnu <sad.boy41@gmail.com>
2021-01-21 16:49:54 +0700

    menambahkan komentar pada file operator_logic.php

--- operator_logic.php
+++ operator_logic.php
@@ -5,17 +5,23 @@
 </head>
 <body>
 	<?php 
+		// Membuat sebuah fungsi dengan nama pembagi
 		function pembagi($nilai_a, $nilai_b){
+			//mendeskripsikan nilai awal
 			$hasil = 0;
+			//membuat kondisi dimana nilai a lebih kecil sama dengan nilai b
 			while($nilai_a >= $nilai_b){
+				// sama dengan $nilai_a = $nilai_a - $nilai_b
 				$nilai_a -= $nilai_b;
 				$hasil++;
 			}
 			return $hasil;
 		}
 
+		//inisialisasi nilai
 		$a = 7;
 		$b = 2;
+		//perintah untuk mencetak hasil dari fungsi pembagian
 		echo "Hasil Bagi ". $a ."/".$b."=".pembagi($a,$b);
 	?>
 </body>
